adf99aba75bffa4caafa5e76318b3ab54c0254f9
Ryan Le <ryan.le@axelspringer.com>
2022-04-20 11:23:01 +0200

    add solution for 3 functions in 3 programming languages

---
+++ src/functions/sum.java
@@ -0,0 +1,11 @@
+public class sum{
+    public static void main (String [] args){
+        int a=2;
+        int b =3;
+        System.out.println(plus(a,b));
+    }
+
+    public static int plus(int num1, int num2){
+        return num1+num2;
+    }
+}
---
+++ src/functions/sum.py
@@ -0,0 +1,8 @@
+def plus(num1,num2):
+    return num1+num2
+    
+class sum:
+    a= 2
+    b= 3
+
+    print(plus(a,b))
